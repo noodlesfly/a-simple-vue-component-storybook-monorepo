4c31dde77faf6c3952cb0a16f3efc3408153d948
wzp <dev_wzp@163.com>
2022-06-17 11:08:01 +0800

    modify code

--- packages/alink/stories/alink.stories.js
+++ packages/alink/stories/alink.stories.js
@@ -9,7 +9,7 @@ export const Alink = _ => ({
   components: { WzpAlink },
   template: `
     <div>
-      <wzp-alink :disabled="false"  href="www.baidu.com">百度</wzp-alink>
+      <wzp-alink :disabled="true"  href="www.baidu.com">百度</wzp-alink>
     </div>
   `
 })
